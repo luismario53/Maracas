e5e0693d080035bcf12cc500c3abd5ef049f2a1a
WhiteArce <47460946+WhiteArce@users.noreply.github.com>
2020-02-01 23:43:19 -0700

    2/1/2020
    Navbar

---
+++ src/components/Ventas.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Ventas extends Component {
+    render(){
+        return(
+            <h1>This is Sales</h1>
+        );
+    }
+}
+
+export default Ventas;
