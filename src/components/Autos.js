dce212a75296fbc7f18e5772c5f69261e0db578f
luismario53 <luismariosandoval44@gmail.com>
2020-02-01 21:15:25 -0700

    header

---
+++ src/components/Autos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Autos extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Inventario Autos</h1>
+            </div>
+        );
+    }
+}
+
+export default Autos;
